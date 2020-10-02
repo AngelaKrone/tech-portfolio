c949daa17f9f5ad521fd551a3c94d6662f55b820
AngelaKrone <angela@littledipper.com>
2020-10-02 15:24:48 -0400

    Step 1 - personalize!

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,11 +12,11 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Angela Krone', // Title for your website.
+    tagline: 'See a need, fill a need.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    url: 'https://angelakrone.github.io', // Your website URL
+    baseUrl: '/test-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'angelakrone',
         twitter: '',
-        github: '',
+        github: 'angelakrone',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
